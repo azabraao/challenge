aae9fc7fb27b1acfbd148932c1413f791c4b31fc
Abraão <az.abraao@gmail.com>
2018-12-11 22:45:41 -0200

    JS created with cart functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,17 @@
+let products = {};
+let total = 0;
+
+$.ajax({
+    url: 'assets/products.json',
+    success: function(data) {
+        products = data;
+    },
+    complete: function() {
+        products.forEach(a => $('.js-products').append(
+            "<div class='last-products__shelf-item js-product' data-price="+a.Value+"><div class='last-products__shelf-item-img'><picture><img src='"+a.images[0].imageUrl+"' class='js-product-image' alt='Last product item'></picture></div><div class='last-products__shelf-item-title'><p class='is-uppercase js-product-name'>"+a.name+"</p></div><div class='last-products__shelf-item-price'><p>R$ <span class='js-item-price'>"+a.Value.toLocaleString()+"</span></p></div></div>"));
+    }
+});
+
 $(document).ready(function(){
     $('.js-hamburguer-toggle').on('click',function(e){
         $('.site-header__responsive-nav').toggle();
@@ -6,5 +20,9 @@ $(document).ready(function(){
     $('.background-overlay').on('click',function(e){
         $('.site-header__responsive-nav').toggle();
         $('.background-overlay').toggle();
+    });
+    $('.js-product').on('click', function(){
+        total += parseFloat(this.dataset.price);
+        $('.js-cart-total').text(total.toLocaleString());
     });
 });
