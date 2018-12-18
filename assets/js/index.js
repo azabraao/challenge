7b3b509062f90a4c55214a8cb89691d0bda9d563
Abraão <az.abraao@gmail.com>
2018-12-18 13:22:56 -0200

    js code improvement

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,23 @@
 let products = {};
 let total = 0;
 
-$.ajax({
-    url: 'assets/products.json',
-    success: function(data) {
-        products = data;
-    },
-    complete: function() {
-        products.forEach(a => $('.js-products').append(
-            "<div class='last-products__shelf-item js-product' data-price="+a.Value+"><div class='last-products__shelf-item-img'><picture><img src='"+a.images[0].imageUrl+"' class='js-product-image' alt='Last product item'></picture></div><div class='last-products__shelf-item-title'><p class='is-uppercase js-product-name'>"+a.name+"</p></div><div class='last-products__shelf-item-price'><p>R$ <span class='js-item-price'>"+a.Value.toLocaleString()+"</span></p></div></div>"));
-    }
-});
+$(document).ready(function(){   
+    $.ajax({
+        url: 'assets/products.json',
+        success: function(data) {
+            products = data;
+            products.forEach(a => $('.js-products').append(
+                "<div class='last-products__shelf-item js-product' data-price="+a.Value+"><div class='last-products__shelf-item-img'><picture><img src='"+a.images[0].imageUrl+"' class='js-product-image' alt='Last product item'></picture></div><div class='last-products__shelf-item-title'><p class='is-uppercase js-product-name'>"+a.name+"</p></div><div class='last-products__shelf-item-price'><p>R$ <span class='js-item-price'>"+a.Value.toLocaleString()+"</span></p></div></div>"));
+        },
+        complete: function() {
+            $('.js-product').on('click', function(){
+                total += parseFloat(this.dataset.price);
+                $('.js-cart-total').text(total.toLocaleString());
+                $('.site-header__cart-total').show();
+            });
+        }
+    });
 
-$(document).ready(function(){
     $('.js-hamburguer-toggle').on('click',function(e){
         $('.site-header__responsive-nav').toggle();
         $('.background-overlay').toggle();
@@ -21,9 +26,5 @@ $(document).ready(function(){
         $('.site-header__responsive-nav').toggle();
         $('.background-overlay').toggle();
     });
-    $('.js-product').on('click', function(){
-        total += parseFloat(this.dataset.price);
-        $('.js-cart-total').text(total.toLocaleString());
-        $('.site-header__cart-total').show();
-    });
+
 });
